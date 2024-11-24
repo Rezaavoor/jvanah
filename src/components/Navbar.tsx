da328354ad7117efc1a2289d010137726cab095f
Reza Hosseini <rezahos@kth.se>
2024-11-24 01:13:10 +0100

    now navbar shows active link/page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import ThemeChanger from "./DarkSwitch";
 import Image from "next/image";
 import {
   Disclosure,
@@ -8,17 +7,21 @@ import {
   DisclosurePanel,
   Transition,
 } from "@headlessui/react";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
+
   useEffect(() => {
     setIsScrolled(window.scrollY > 50);
   }, []);
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      setIsScrolled(scrollTop > 50); // Change 50 to whatever scroll threshold you want
+      setIsScrolled(scrollTop > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -27,31 +30,32 @@ export const Navbar = () => {
     };
   }, []);
 
+  const menuItems = [
+    { href: "/", label: "Home" },
+    { href: "/whatwedo", label: "What We Do" },
+    { href: "/support", label: "How To Support" },
+    { href: "/about", label: "About Us" },
+    { href: "/become-a-member", label: "Become a Member", isButton: true },
+  ];
+
   return (
     <div
-      className={`w-screen fixed transition-all duration-500 ease-in-out bg-lighterGreen
-      ${isScrolled ? "border-b-[1px] border-primaryGreen" : "border-none"}
-       z-50`}
+      className={`w-screen fixed transition-all duration-500 ease-in-out bg-lighterGreen ${
+        isScrolled ? "border-b-[1px] border-primaryGreen" : "border-none"
+      } z-50`}
     >
       <nav className="container relative flex flex-wrap items-center justify-between p-2 lg:p-4 mx-auto lg:justify-between xl:px-1">
-        {/* Logo  */}
+        {/* Logo */}
         <Link href="/">
-          <span className="flex items-end space-x-2 text-2xl font-medium text-primary transition-all duration-500 ease-in-out hover:scale-125 relative w-14 h-14 ">
-            <span>
-              <Image
-                src="/img/jvanah/logo.svg"
-                fill={true}
-                alt="N"
-                className="w-16"
-              />
-            </span>
+          <span className="flex items-end space-x-2 text-2xl font-medium text-primary transition-all duration-500 ease-in-out hover:scale-125 relative w-14 h-14">
+            <Image
+              src="/img/jvanah/logo.svg"
+              fill={true}
+              alt="Logo"
+              className="w-16"
+            />
           </span>
         </Link>
-
-        {/* get started  */}
-        {/* <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
-          <ThemeChanger />
-        </div> */}
 
         <Disclosure>
           {({ open, close }) => (
@@ -65,14 +69,13 @@ export const Navbar = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
-                  {open && (
+                  {open ? (
                     <path
                       fillRule="evenodd"
                       clipRule="evenodd"
                       d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
                     />
-                  )}
-                  {!open && (
+                  ) : (
                     <path
                       fillRule="evenodd"
                       d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
@@ -91,60 +94,39 @@ export const Navbar = () => {
                 leaveTo="transform scale-95 opacity-0"
               >
                 <DisclosurePanel className="flex flex-col w-full mt-2 bg-lightGreen shadow-lg rounded-lg lg:hidden">
-                  <Link
-                    href="/about"
-                    onClick={() => close()}
-                    className="w-full px-4 py-2 text-primary hover:bg-gray-100 focus:bg-gray-100 rounded-t-lg"
-                  >
-                    About Us
-                  </Link>
-                  <Link
-                    href="/"
-                    onClick={() => close()}
-                    className="w-full px-4 py-2 text-primary hover:bg-gray-100 focus:bg-gray-100"
-                  >
-                    What We Do
-                  </Link>
-                  <Link
-                    href="/"
-                    onClick={() => close()}
-                    className="w-full px-4 py-2 text-primary hover:bg-gray-100 focus:bg-gray-100 rounded-b-lg"
-                  >
-                    How To Support
-                  </Link>
+                  {menuItems.map((item) => (
+                    <Link
+                      key={item.href}
+                      href={item.href}
+                      onClick={() => close()}
+                      className={`w-full px-4 py-2 text-primary hover:bg-gray-100 focus:bg-gray-100 ${
+                        pathname === item.href ? "font-bold" : ""
+                      } ${item.isButton ? "underline" : ""}`}
+                    >
+                      {item.label}
+                    </Link>
+                  ))}
                 </DisclosurePanel>
               </Transition>
             </>
           )}
         </Disclosure>
 
-        {/* menu  */}
+        {/* Desktop Menu */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            <li className="mr-3 nav__item">
-              <Link
-                href="/about"
-                className="inline-block px-4 py-2 text-lg font-normal text-primary no-underline rounded-md  hover:bg-primary hover:text-lighterGreen focus:text-lighterGreen focus:bg-primary focus:outline-none transition-all duration-200 ease-in-out"
-              >
-                {"About Us"}
-              </Link>
-            </li>
-            <li className="mr-3 nav__item">
-              <Link
-                href="/"
-                className="inline-block px-4 py-2 text-lg font-normal text-primary no-underline rounded-md  hover:bg-primary hover:text-lighterGreen focus:text-lighterGreen focus:bg-primary focus:outline-none transition-all duration-200 ease-in-out"
-              >
-                {"What We Do"}
-              </Link>
-            </li>
-            <li className="mr-3 nav__item">
-              <Link
-                href="/"
-                className="inline-block px-4 py-2 text-lg font-normal text-primary no-underline rounded-md  hover:bg-primary hover:text-lighterGreen focus:text-lighterGreen focus:bg-primary focus:outline-none transition-all duration-200 ease-in-out"
-              >
-                {"How To Support"}
-              </Link>
-            </li>
+            {menuItems.map((item) => (
+              <li key={item.href} className="mr-3 nav__item">
+                <Link
+                  href={item.href}
+                  className={`inline-block px-2 py-1 text-base font-normal text-primary no-underline rounded-md hover:bg-primary hover:text-lighterGreen transition-all duration-200 ease-in-out ${
+                    pathname === item.href ? "font-bold text-primaryGreen" : ""
+                  } ${item.isButton ? "font-semibold underline" : ""}`}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
